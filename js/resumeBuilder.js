7c8c5f18d2ef95f84c89dbc09d241e433d228261
Alexandra Polubiec <apolubi@alexandras-mbp.attlocal.net>
2015-09-06 07:22:03 -0700

    added name & role to page

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,9 @@
-$("#main").append(["Alex Polubiec"]);
+var name = "Alex Polubiec";
+var role = "Front-End Web Developer & Technical Writer";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
