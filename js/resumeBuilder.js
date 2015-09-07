28d0983d5c9370b49e532f6ff9fae649a0229fd7
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2015-09-06 20:54:22 -0700

    added name, role, education

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,9 +1,42 @@
 var name = "Alex Polubiec";
 var role = "Front-End Web Developer & Technical Writer";
 
-var formattedName = HTMLheaderName.replace("%data%", name);
+var bio = {
+	"name": name,
+	"role": role,
+	"contact": "apolubi@gmail.com",
+	"picture": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/0ac/1b3/2e622ce.jpg",
+	"welcome": "Hi there! I'm Alex.",
+	"skills": ["Python", "HTML", "CSS", "JavaScript", "jQuery", "Technical Documentation"]
+};
 
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+var work = {};
+work.position = "Senior Technical Writer";
+work.employer = "Shape Security, Inc.";
+work.years = "0.5";
+work.city = "Mountain View";
 
-$("#header").append(formattedName);
-$("#header").append(formattedRole);
+var education = {};
+	"Udacity": {
+		"years": "2015-present",
+		"majors": ["Front-End Web Development"]
+	},
+	"Skillcrush": {
+		"years": "2014",
+		"majors": ["Web Development Blueprint"]
+	},
+	"Bennington College": {
+		"years": "2007-2011",
+		"city": "Bennington, VT",
+		"majors": ["Music", "Drama"]
+	}
+};
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+
+var formattedRole = HTMLheaderRole.replace("%data%", bio["role"]);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#main").append(work["position"]);
+$("#main").append(education.name);
