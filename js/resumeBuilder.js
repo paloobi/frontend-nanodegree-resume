6707ef2b0553686ebe37b35c8e1e80585b95b048
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2015-09-10 20:20:35 -0700

    fixed it so that work actually displays now

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,122 +1,130 @@
 var bio = {
-	"name": "Alex Polubiec",
-	"role": "Front-End Web Developer & Technical Writer",
-	"contacts": {
-		"email": "alexandra.polubiec@gmail.com",
-		"mobile": "914-330-1329",
-		"github": "paloobi",
-		"twitter": "@AlliePaloobi",
-		"location": "SF Bay Area"
-	},
-	"picture": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/0ac/1b3/2e622ce.jpg",
-	"welcomeMessage": "Hi there! I'm Alex.",
-	"skills": [
-		"Python",
-		"HTML",
-		"CSS",
-		"JavaScript",
-		"jQuery",
-		"Technical Documentation"
-	]
+  "name": "Alex Polubiec",
+  "role": "Front-End Web Developer & Technical Writer",
+  "contacts": {
+      "email": "alexandra.polubiec@gmail.com",
+      "mobile": "914-330-1329",
+      "github": "paloobi",
+      "twitter": "@AlliePaloobi",
+      "location": "SF Bay Area"
+    },
+  "picture": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/0ac/1b3/2e622ce.jpg",
+  "welcomeMessage": "Hi there! I'm Alex.",
+  "skills": [
+      "Python",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "jQuery",
+      "Technical Documentation"
+    ]
 };
 
 var work = {
-	"jobs": [
-		{
-			"title": "Senior Technical Writer",
-			"employer": "Shape Security",
-			"dates": "2015 - Present",
-			"city": "Mountain View",
-			"description": [
-				"Translate PDF documentation into single-sourced HTML and PDF docs using Sphinx documentation tool",
-				"Create and maintain custom Python build script for the Technical Documentation Library",
-				"Work with engineering team to create automated documentation",
-				"Collaborate with engineering, support, and product teams to provide accurate documentation for the ShapeShifter Botwall"
-			]
-		},
-		{
-			"title": "Senior Technical Writer/Technical Writer",
-			"employer": "A10 Networks",
-			"years": "2013 - 2015",
-			"city": "San Jose",
-			"description": [
-				"Authored user guide & automated documentation for RESTful API.",
-				"Provided solution for transitioning from PDF to HTML documentation.",
-				"Implemented new context-sensitive help system using FrameMaker HTML output, with custom CSS and JavaScript topic mapping, while managing the GUI documentation for a major release.",
-				"Restructured and managed Scripting Language Reference & System Administration Guide"
-			]
-		}
-	]
+  "jobs": [
+    {
+      "title": "Senior Technical Writer",
+      "employer": "Shape Security",
+      "dates": "2015 - Present",
+      "location": "Mountain View",
+      "description": "Translate PDF documentation into single-sourced HTML and PDF docs using Sphinx documentation tool. Create and maintain custom Python build script for the Technical Documentation Library. Work with engineering team to create automated documentation. Collaborate with engineering, support, and product teams to provide accurate documentation for the ShapeShifter Botwall."
+    },
+    {
+      "title": "Senior Technical Writer/Technical Writer",
+      "employer": "A10 Networks",
+      "dates": "2013 - 2015",
+      "location": "San Jose",
+      "description": "Authored user guide & automated documentation for RESTful API. Provided solution for transitioning from PDF to HTML documentation. Implemented new context-sensitive help system using FrameMaker HTML output, with custom CSS and JavaScript topic mapping, while managing the GUI documentation for a major release. Restructured and managed Scripting Language Reference & System Administration Guide."
+    }
+  ]
 };
 
 var education = {
-	"schools": [
-		{	
-			"name": "Udacity",
-			"years": "2015-present",
-			"degree": "Nanodegree",
-			"majors": ["Front-End Web Development"]
-		},
-		{
-			"name": "Skillcrush",
-			"years": "2014",
-			"degree": ["Web Developer Blueprint"]
-		},
-		{	
-			"name": "Bennington College",
-			"years": "2007-2011",
-			"city": "Bennington, VT",
-			"degree": "Bachelor of Arts"
-			"majors": ["Music", "Drama"]
-		}
-	],
-	"onlineCourses": [
-		{
-			"title": "Introduction to Computer Science and Programming with Python",
-			"school": "MITx",
-			"year": "2013",
-			"url": "http://www.edx.org/"
-		},
-		{
-			"title": "Introduction to Statistics",
-			"school": "BerkeleyX",
-			"year": "2012",
-			"url": "http://www.edx.org"
-		}
-	]
+  "schools": [
+    {
+      "name": "Udacity",
+      "years": "2015-present",
+      "degree": "Nanodegree",
+      "majors": ["Front-End Web Development"]
+    },
+    {
+      "name": "Skillcrush",
+      "years": "2014",
+      "degree": ["Web Developer Blueprint"]
+    },
+    {
+      "name": "Bennington College",
+      "years": "2007-2011",
+      "city": "Bennington, VT",
+      "degree": "Bachelor of Arts",
+      "majors": ["Music", "Drama"]
+    }
+  ],
+  "onlineCourses": [
+    {
+      "title": "Introduction to Computer Science and Programming with Python",
+      "school": "MITx",
+      "year": "2013",
+      "url": "http://www.edx.org/"
+    },
+    {
+      "title": "Introduction to Statistics",
+      "school": "BerkeleyX",
+      "year": "2012",
+      "url": "http://www.edx.org"
+    }
+  ]
 };
 
 var projects = {
-	"projects": [
-		{
-			"title": "Online Resume",
-			"dates": "2015",
-			"description": "Implemented online resume with Bootstrap",
-			"images": []
-		}
-	]
+  "projects": [
+    {
+      "title": "Online Resume",
+      "dates": "2015",
+      "description": "Implemented online resume with Bootstrap",
+      "images": []
+    }
+  ]
 };
 
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
-
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
-for (job in work.jobs) {
-	$("#workExperience").append(HTMLworkStart);
-	var formattedEmployer = HTMLworkEmployer.replace("%data", item.employer);
-	var formattedTitle = HTMLworkTitle.replace("%data%", item.title);
-	var formattedEmployerTitle = formattedEmployer + formattedTitle;
-	$(".work-entry:last").append(formattedEmployerTitle);
+function displayWork() {
+	for (job in work.jobs) {
+	  $("#workExperience").append(HTMLworkStart);
+
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+	  $(".work-entry:last").append(formattedEmployerTitle);
+
+	  var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+	  $(".work-entry:last").append(formattedDates);
+	  
+	  var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+	  $(".work-entry:last").append(formattedLocation);
+
+	  var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+	  $(".work-entry:last").append(formattedDescription);
+	}
 };
 
-if (bio.skills.length > 0) {
-	$("#header").append(HTMLskillsStart);
-};
+function displaySkills() {
+	if (bio.skills.length > 0) {
+	  $("#header").append(HTMLskillsStart);
+	};
 
-for (var skill in bio.skills) {
-	var formattedSkill = HTMLskills.replace("%data%", bio.skills[i])
-	$("#skills").append(formattedSkill);
+	for (var skill in bio.skills) {
+	  var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill])
+	  $("#skills").append(formattedSkill);
+	}
 }
+
+displaySkills()
+displayWork()
+
+
