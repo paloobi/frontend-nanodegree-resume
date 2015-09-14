d4e76f135c0ce8bdb805d4de003c15e17ecaae03
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2015-09-13 19:04:36 -0700

    added  contacts to footer

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -44,18 +44,24 @@ var bio = {
 
     var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
     $("#topContacts").append(formattedEmail);
+    $("#footerContacts").append(formattedEmail);
 
     var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
     $("#topContacts").append(formattedMobile);
+    $("#footerContacts").append(formattedMobile);
 
     var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
     $("#topContacts").append(formattedGithub);
+    $("#footerContacts").append(formattedGithub);
 
     var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
     $("#topContacts").append(formattedTwitter);
+    $("#footerContacts").append(formattedTwitter);
 
     var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
     $("#topContacts").append(formattedLocation);
+    $("#footerContacts").append(formattedLocation);
+
   }
 
 };
